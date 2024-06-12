ad7b7346042128f62d72cdef3602bc314c5ed38e
Junior Alcantara <jrlevita09@hotmail.com>
2024-06-12 18:37:24 -0300

    For com Array + Exemplo Break e Continua

---
+++ CONTROLE-FLUXO/.history/src/ExemploArray_20240612183017.java
@@ -0,0 +1,9 @@
+public class ExemploArray {
+    public static void main(String[] args) {
+        String carros[] = {"Gol", "Agile", "Celta", "Civic"};
+
+        for (String carro : carros){
+            System.out.println("O carro é um : " + carro);
+        }
+    }
+}
---
+++ CONTROLE-FLUXO/src/ExemploBreakContinua.java
@@ -0,0 +1,9 @@
+public class ExemploBreakContinua {
+    public static void main(String[] args) {
+        for (int numero = 1; numero <= 10; numero ++){
+            if (numero == 7)
+                continue;
+            System.out.println(numero);
+        }
+    }
+}
